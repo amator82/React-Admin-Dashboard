2a628e42e00b93befcb6ad419cdb34b82f7fae4c
amator82 <ilyakorckishko@gmail.com>
2023-03-25 21:33:16 +0200

    add light and dark themes for app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import './App.css'
+import React from 'react'
+
+import { ColorMdoeContext, useMode } from './theme'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 
 function App() {
-    return <div className='App'></div>
+    const [theme, colorMode] = useMode()
+    return (
+        <ColorMdoeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <div className='app'>
+                    <main className='content'></main>
+                </div>
+            </ThemeProvider>
+        </ColorMdoeContext.Provider>
+    )
 }
 
 export default App
