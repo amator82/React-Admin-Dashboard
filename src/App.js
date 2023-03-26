0cd2902e0a01ff5e3f7ddf0c3aedd3cb10afa33d
amator82 <ilyakorckishko@gmail.com>
2023-03-26 12:34:08 +0300

    add app router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Routes, Route, Form } from 'react-router-dom'
 
 import { ColorMdoeContext, useMode } from './theme'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 
 import TopBar from './scenes/global/Topbar'
+import SideBar from './scenes/global/Sidebar';
+
+import Dashboard from './scenes/dashboard'
+import Team from './scenes/team'
+import Contacts from './scenes/contacts'
+import Invoices from './scenes/invoices'
+import Bar from './scenes/bar'
+import Pie from './scenes/pie'
+import Line from './scenes/line'
+import FAQ from './scenes/faq'
+import Calendar from './scenes/calendar'
+import Geography from './scenes/geography'
 
 function App() {
     const [theme, colorMode] = useMode()
@@ -12,8 +25,22 @@ function App() {
             <ThemeProvider theme={theme}>
                 <CssBaseline />
                 <div className='app'>
+                    <SideBar/>
                     <main className='content'>
-                        <TopBar/>
+                        <TopBar />
+                        <Routes>
+                            <Route path='/' element={<Dashboard />} />
+                            <Route path='/team' element={<Team />} />
+                            <Route path='/contacts' element={<Contacts />} />
+                            <Route path='/invoices' element={<Invoices />} />
+                            <Route path='/form' element={<Form />} />
+                            <Route path='/bar' element={<Bar />} />
+                            <Route path='/pie' element={<Pie />} />
+                            <Route path='/line' element={<Line />} />
+                            <Route path='/faq' element={<FAQ />} />
+                            <Route path='/geography' element={<Geography />} />
+                            <Route path='/calendar' element={<Calendar />} />
+                        </Routes>
                     </main>
                 </div>
             </ThemeProvider>
