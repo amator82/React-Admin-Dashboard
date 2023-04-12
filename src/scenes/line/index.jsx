1b58277b75c706b63fdd966e6b87bfd1a8a9ade0
amator82 <ilyakorckishko@gmail.com>
2023-04-12 13:41:23 +0300

    change Line scene title

--- src/scenes/line/index.jsx
+++ src/scenes/line/index.jsx
@@ -8,7 +8,7 @@ import LineChart from '../../components/LineChart'
 const Line = () => {
     return (
         <Box m='20px'>
-            <Header title='Bar Chart' subtitle='Simple Bar Chart' />
+            <Header title='Line Chart' subtitle='Simple Line Chart' />
             <Box height='75vh'>
                 <LineChart />
             </Box>
