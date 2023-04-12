bd64b2a19fa90b184b1c83b9eb28405acf482056
amator82 <ilyakorckishko@gmail.com>
2023-04-12 12:43:26 +0300

    add page faq accordions

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -1,7 +1,98 @@
 import React from 'react'
 
+import {
+    Box,
+    Accordion,
+    useTheme,
+    AccordionSummary,
+    AccordionDetails,
+    Typography
+} from '@mui/material'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+
+import Header from '../../components/Header'
+
+import { tokens } from '../../theme'
+
 const FAQ = () => {
-    return <div>FAQ</div>
+    const theme = useTheme()
+    const colors = tokens(theme.palette.mode)
+
+    return (
+        <Box m='20px'>
+            <Header title='FAQ' subtitle='Frequenly Asked Questions Page' />
+            <Accordion defaultExpanded>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography color={colors.greenAccent[500]} variant='h5'>
+                        An Important Question
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Officiis sed asperiores voluptates consectetur,
+                        aliquid cupiditate possimus mollitia quis ex vel!
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion defaultExpanded>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography color={colors.greenAccent[500]} variant='h5'>
+                        Second Important Question
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Officiis sed asperiores voluptates consectetur,
+                        aliquid cupiditate possimus mollitia quis ex vel!
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion defaultExpanded>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography color={colors.greenAccent[500]} variant='h5'>
+                        The Final Question
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Officiis sed asperiores voluptates consectetur,
+                        aliquid cupiditate possimus mollitia quis ex vel!
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion defaultExpanded>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography color={colors.greenAccent[500]} variant='h5'>
+                        Some Question
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Officiis sed asperiores voluptates consectetur,
+                        aliquid cupiditate possimus mollitia quis ex vel!
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion defaultExpanded>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography color={colors.greenAccent[500]} variant='h5'>
+                        Another Question
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Officiis sed asperiores voluptates consectetur,
+                        aliquid cupiditate possimus mollitia quis ex vel!
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+        </Box>
+    )
 }
 
 export default FAQ
